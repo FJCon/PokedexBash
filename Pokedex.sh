11c8c8f7cd1b8bf38bd5a3ca8c56d2fb07204686
FJCon <conilfernando@gmail.com>
2023-05-22 12:09:05 -0300

    prueba script

--- Pokedex.sh
+++ Pokedex.sh
@@ -1,6 +1,14 @@
+#!/bin/bash
+# Es neceario tener instalado jq
+# sudo apt-get install jq
+
 echo "============================================="
 echo "--------P O K E D E X   E N   B A S H--------"
 echo "============================================="
 read -p "Ingrese nombre del pokemon: " pokemon
-curl -r https://pokeapi.co/api/v2/pokemon/$pokemon | jq ''
-
+curl -s https://pokeapi.co/api/v2/pokemon/$pokemon | jq '.id' { read -r $id }
+echo "ID: $id"
+curl -s https://pokeapi.co/api/v2/pokemon/$pokemon | jq '.name' { read -r $nombre }
+echo "NOMBRE : $nombre"
+curl -s https://pokeapi.co/api/v2/pokemon/charmander | jq '.types[0].type.name' { read -r $tipo } 
+echo "TIPO : $tipo"
